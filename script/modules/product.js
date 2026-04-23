566515c9c895be17d589b788dc8106cb180d7d47
jeger8byte <rizkialpajri96678@gmail.com>
2026-04-23 14:21:45 +0700

    perubahan kecil

--- script/modules/product.js
+++ script/modules/product.js
@@ -54,6 +54,8 @@ try{
     const response = await fetch("/Darikebun/php/getCart.php");
 
     const data = await response.json();
+    
+   
     const jumlah_item = data.length;
 
     let cartIconQuantity = document.querySelector('.cart-quantity')
